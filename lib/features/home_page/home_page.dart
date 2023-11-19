a92d6857cd4c03d0dd3037af8db3c2d9beaf087d
Andrey <83059277+Pib0di@users.noreply.github.com>
2023-11-19 15:53:25 +1000

    layouts: home page

---
+++ lib/features/home_page/home_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:notepad_weather/features/home_page/service/home_page_service.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final HomePageService homePageService = HomePageService();
+
+  @override
+  void initState() {
+    super.initState();
+    homePageService.initial();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Погодный календарь'),
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.question_mark_outlined),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.more_vert_outlined),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      body: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: homePageService.cardMonth.length,
+          padding: const EdgeInsets.all(8.0),
+          controller: ScrollController(
+            //todo сделать чтобы список скролился с середины (GlobalKey or Bloc)
+            // middle of the list
+            initialScrollOffset: (homePageService.cardMonth.length * 402) / 2,
+          ),
+          itemBuilder: (BuildContext context, int index) {
+            return homePageService.cardMonth[index];
+          }),
+    );
+  }
+}
